12833e8b6b497d4f3aa588a777047b2eedafedf2
= <=>
2020-08-03 16:22:49 -0300

    feature: estilização da rota /study para mobile

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
 import './styles.css';
 
 function TeacherList() {
     return (
         <div id="page-teacher-list" className="container">
-            <PageHeader title="Esses são os proffys disponíveis.">
+            <PageHeader title="Estes são os proffys disponíveis.">
                 <form id="search-teachers">
                     <div className="input-block">
                         <label htmlFor="subject">Matéria</label>
@@ -25,6 +27,35 @@ function TeacherList() {
                     </div>
                 </form>
             </PageHeader>
+
+            <main>
+                <article className="teacher-item">
+                    <header>
+                        <img src="https://avatars0.githubusercontent.com/u/33378274?s=460&u=35316d2855c783496986fce7512ad6cce75bf890&v=4" alt="Joilson Leal"/>
+                        <div>
+                            <strong>Joilson Leal</strong>
+                            <span>Química</span>
+                        </div>
+                    </header>
+
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        <br/><br/>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur doloremque tempora quasi tempore expedita assumenda possimus, recusandae in laudantium, facilis fuga repellat voluptas soluta earum maiores molestiae voluptatem, dolores beatae!
+                    </p>
+
+                    <footer>
+                        <p>
+                            Preço/hora
+                            <strong>R$ 80,00</strong>
+                        </p>
+                        <button type="button">
+                            <img src={whatsappIcon} alt="Whatsapp" />
+                            Entrar em contato
+                        </button>
+                    </footer>
+                </article>
+            </main>
         </div>
     );
 }
