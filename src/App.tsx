52aa4f50d8a51d38f07e7f7446512ae6ccd0cdd4
= <=>
2020-08-03 09:06:35 -0300

    feature: adição de estilo global

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import './assets/styles/global.css';
+
 function App() {
   return (
     <div className="App">
-      
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde reiciendis perspiciatis enim ipsa nemo non deserunt, praesentium sed? Nulla illum nihil reiciendis, itaque rem corrupti esse suscipit veritatis explicabo quaerat?
     </div>
   );
 }
